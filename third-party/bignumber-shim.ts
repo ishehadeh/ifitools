50d3081fce70b6ce7bf419ecf14d77fac569c6f5
Ian Shehadeh <ian@shehadeh.net>
2024-05-25 10:52:02 -0400

    fix bignumber url

--- third-party/bignumber-shim.ts
+++ third-party/bignumber-shim.ts
@@ -1,4 +1,5 @@
 // @deno-types="https://raw.githubusercontent.com/MikeMcl/bignumber.js/v9.1.2/bignumber.d.ts"
-export * from "bignumber";
-import BigNumber from "bignumber";
+export * from "https://raw.githubusercontent.com/MikeMcl/bignumber.js/v9.1.2/bignumber.mjs";
+// @deno-types="https://raw.githubusercontent.com/MikeMcl/bignumber.js/v9.1.2/bignumber.d.ts"
+import BigNumber from "https://raw.githubusercontent.com/MikeMcl/bignumber.js/v9.1.2/bignumber.mjs";
 export default BigNumber;
